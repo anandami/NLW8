486ea1acfebbeb7dac041758266c374da8a2322a
anandami <ananda.anm@gmail.com>
2022-05-08 14:54:30 -0300

    Exercícios de uso do tailwind e propriedades do react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+interface ButtonProps{
+  text?: string;
+}
+
+
+function Button(props: ButtonProps){
+  return <button className='button'>{props.text ?? 'Default'}</button>
+}
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+  <div className="flex gap-2">
+    <Button text = "Enviar"/>
+    <Button text = "OK"/>
+    <Button/>
+  </div>
+  )
 }
 
 export default App
